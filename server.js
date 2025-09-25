e39453e8c229cfa5573a50665419202b9f9d2148
Danielle Morvant <daniellemorvant1@gmail.com>
2025-09-25 19:49:40 +0000

    working

--- server.js
+++ server.js
@@ -3,10 +3,32 @@ const express = require("express");
 const app = express();
 const PORT = 3000;
 
-app.get("/", function (req, res) {
-    res.send("<pre>Baby's First Homemade Purees</pre>");
+app.get("/", (req, res) => {
+    res.send(`
+        <html>
+        <pre>list of route servers
+               /multiply
+               /divide
+               /subtract</pre>
+               </html>`)
 });
 
-app.listen(PORT, ( ) => {
+app.get("/multiply", function (req, res) {
+    let a = 25;
+    let b = 4;
+    output = 25*4
+    console.log(output)
+    res.send("<h1>Let's Do The Math By The Multitudes! + output</h1>");
+});
+
+app.get("/divide", function (req, res) {
+    res.send("<h1>Let's Do The Math Without Dividing The House!</h1>");
+});
+
+app.get("/subtract", function (req, res) {
+    res.send("<h1>Let's Do The Math !</h1>");
+});
+
+app.listen(PORT, () => {
     console.log("It's Alive!!!")
 })
